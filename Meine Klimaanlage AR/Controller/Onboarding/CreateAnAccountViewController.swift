979e4231efd4bbd299166715038fc1c4e58d0e58
Martin Maly <martinmaly66@hotmail.com>
2020-06-07 18:02:56 -0400

    Hide navigation bar appropriately

--- Meine Klimaanlage AR/Controller/Onboarding/CreateAnAccountViewController.swift
+++ Meine Klimaanlage AR/Controller/Onboarding/CreateAnAccountViewController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class CreateAnAccountViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    */
-
+    
+    //MARK: - Actions
+    
+    @IBAction func userPressedCreateAccount(_ sender: Any) {
+    }
+    
 }
