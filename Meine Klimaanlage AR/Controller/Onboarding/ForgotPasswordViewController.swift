9b889d839d34c59806a6aae6832643e618b66985
Martin Maly <martinmaly66@hotmail.com>
2020-06-07 18:12:51 -0400

    Added engine for ForgotPasswordViewController
    cont.

---
+++ Meine Klimaanlage AR/Controller/Onboarding/ForgotPasswordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ForgotPasswordViewController.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2020-06-07.
+//  Copyright © 2020 Tim Kohmann. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Actions
+    @IBAction func userPressedSendRecoveryLink(_ sender: Any) {
+    }
+}
+ 
