ed343631903c62c663c0bbe007dd606211164615
Martin Maly <martinmaly66@hotmail.com>
2020-06-07 14:57:38 -0400

    Initial Commit

---
+++ Meine Klimaanlage AR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2020-06-07.
+//  Copyright © 2020 Tim Kohmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
