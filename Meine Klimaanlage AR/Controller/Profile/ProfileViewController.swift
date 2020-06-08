3246fe7a2c1e32d02bcde15c690baf82b53fe680
Martin Maly <40171261+martinmaly21@users.noreply.github.com>
2020-06-07 23:01:36 -0400

    Revert "Develop"

---
+++ Meine Klimaanlage AR/Controller/Profile/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2020-06-07.
+//  Copyright © 2020 Tim Kohmann. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
