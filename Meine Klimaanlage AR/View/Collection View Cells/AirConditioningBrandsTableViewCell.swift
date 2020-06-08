602311bcc54c72891db76286008cf04d57c1e5d6
Martin Maly <martinmaly66@hotmail.com>
2020-06-07 20:21:24 -0400

    Hooked up engines for profile and home

---
+++ Meine Klimaanlage AR/View/Collection View Cells/AirConditioningBrandsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AirConditioningBrandsTableViewCell.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2020-06-07.
+//  Copyright © 2020 Tim Kohmann. All rights reserved.
+//
+
+import UIKit
+
+class AirConditioningBrandsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
