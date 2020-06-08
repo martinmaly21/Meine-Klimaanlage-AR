09a58cde67b91500ac22a7054843aa0cf851bd04
Martin Maly <martinmaly66@hotmail.com>
2020-06-07 23:54:02 -0400

    User can now log in

--- Meine Klimaanlage AR/Controller/Onboarding/LogInViewController.swift
+++ Meine Klimaanlage AR/Controller/Onboarding/LogInViewController.swift
@@ -7,11 +7,44 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LogInViewController: UIViewController {
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUI()
+        addGestureRecognizers()
+        addNotificationObserver()
+    }
+    
+    private func setUpUI() {
+        logInButton.layer.cornerRadius = logInButton.frame.height / 2
+    }
+    
+    private func addGestureRecognizers() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapScreen))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    private func addNotificationObserver() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillChangeFrameNotification,
+            object: nil
+        )
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -20,8 +53,42 @@ class LogInViewController: UIViewController {
     }
     
     //MARK: - Actions
-
+    
+    @objc func didTapScreen() {
+        view.endEditing(true)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            scrollView.contentInset = .zero
+            scrollView.scrollIndicatorInsets = .zero
+        } else {
+            let inset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+            scrollView.contentInset = inset
+            scrollView.scrollIndicatorInsets = inset
+        }
+    }
+    
     @IBAction func userDidPressLogin(_ sender: Any) {
-        performSegue(withIdentifier: "logInSegue", sender: self)
+        guard let email = emailTextField.text, !email.isEmpty,
+            let password = passwordTextField.text, !password.isEmpty else {
+                ErrorManager.showOnboardingError(with: .missingfields, on: self)
+                return
+        }
+        
+        Auth.auth().signIn(
+            withEmail: email,
+            password: password) { (resultt, error) in
+                if let error = error {
+                    ErrorManager.showFirebaseError(with: error.localizedDescription, on: self)
+                } else {
+                    self.performSegue(withIdentifier: "logInSegue", sender: self)
+                }
+        }
     }
 }
--- Meine Klimaanlage AR/Manager/ErrorManager.swift
+++ Meine Klimaanlage AR/Manager/ErrorManager.swift
@@ -21,10 +21,6 @@ struct Errors {
         case passwordsDontMatch
         case passwordNotStrongEnough
         
-        //logging in
-        case emailNotInSystem
-        case incorrectPassword
-        
         //if user does not fill out all fields
         case missingfields
     }
@@ -42,10 +38,6 @@ class ErrorManager {
             errorText = "The passwords you have entered don't match."
         case .passwordNotStrongEnough:
             errorText = "The password you entered isn't strong enough. It must be at least 8 characters, contain one symbol, one number, and one upper case letter. "
-        case .emailNotInSystem:
-            errorText = "The email you entered is not in our system. Please sign up instead."
-        case .incorrectPassword:
-            errorText = "The password you entered is incorrect. Please try again."
         case .missingfields:
             errorText = "One or more of the fields is empty. Please fill them out, and try again."
         }
