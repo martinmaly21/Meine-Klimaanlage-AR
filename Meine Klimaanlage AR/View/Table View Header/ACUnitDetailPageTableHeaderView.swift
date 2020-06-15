848bf547ffe9060c6c49d9f4d5ad80b71553c70e
Martin Maly <martinmaly66@hotmail.com>
2020-06-14 21:55:06 -0400

    Added table header view for AC unit detail

---
+++ Meine Klimaanlage AR/View/Table View Header/ACUnitDetailPageTableHeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  ACUnitDetailPageTableHeaderView.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2020-06-14.
+//  Copyright © 2020 Tim Kohmann. All rights reserved.
+//
+
+import UIKit
+
+protocol ACUnitDetailPageTableHeaderViewDelegate: class {
+    func userChangedACUnitEnvironmentType(with newType: ACUnitEnvironmentType)
+}
+
+class ACUnitDetailPageTableHeaderView: UIView {
+    @IBOutlet weak var ACUnitHeaderImageView: UIImageView!
+    @IBOutlet weak var ACUnitHeaderSegmentedControl: UISegmentedControl!
+    public weak var delegate: ACUnitDetailPageTableHeaderViewDelegate?
+    
+    @IBAction func segmentedControllerDidChange(_ sender: UISegmentedControl) {
+        let newACUnitEnvironmentType: ACUnitEnvironmentType = sender.selectedSegmentIndex == 0 ? .exterior : .interior
+        delegate?.userChangedACUnitEnvironmentType(with: newACUnitEnvironmentType)
+    }
+}
