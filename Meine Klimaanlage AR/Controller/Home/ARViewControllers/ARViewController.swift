288aa08fd9dbaf5bde068de27e8c4a1cc8477b1b
Martin Maly <martinmaly66@hotmail.com>
2020-07-08 21:48:11 -0400

    Added new files

--- Meine Klimaanlage AR/Controller/Home/ARViewControllers/ARViewController.swift
+++ Meine Klimaanlage AR/Controller/Home/ARViewControllers/ARViewController.swift
@@ -20,6 +20,27 @@ class ARViewController: UIViewController {
     
     internal var focusSquare = FocusSquare()
     
+    
+    
+    //MARK: - ARKit Configuration Properties
+    
+    /// A type which manages gesture manipulation of virtual content in the scene.
+     lazy var virtualObjectInteraction = VirtualObjectInteraction(sceneView: sceneView, viewController: self)
+     
+     /// Coordinates the loading and unloading of reference nodes for virtual objects.
+     let virtualObjectLoader = VirtualObjectLoader()
+     
+     /// Marks if the AR experience is available for restart.
+     var isRestartAvailable = true
+     
+     /// A serial queue used to coordinate adding or removing nodes from the scene.
+     let updateQueue = DispatchQueue(label: "com.example.apple-samplecode.arkitexample.serialSceneKitQueue")
+     
+     /// Convenience accessor for the session owned by ARSCNView.
+     var session: ARSession {
+         return sceneView.session
+     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpUI()
@@ -37,6 +58,19 @@ class ARViewController: UIViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        // Prevent the screen from being dimmed to avoid interuppting the AR experience.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // Start the `ARSession`.
+        resetTracking()
+    }
+    
+    
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         tabBarController?.tabBar.isHidden = true
@@ -53,8 +87,72 @@ class ARViewController: UIViewController {
         saveButton.isEnabled = false
         navigationItem.rightBarButtonItem = saveButton
     }
-
     
+    // MARK: - Session management
+       
+       /// Creates a new AR configuration to run on the `session`.
+       func resetTracking() {
+           virtualObjectInteraction.selectedObject = nil
+           
+           let configuration = ARWorldTrackingConfiguration()
+           configuration.planeDetection = [.horizontal, .vertical]
+           if #available(iOS 12.0, *) {
+               configuration.environmentTexturing = .automatic
+           }
+           session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+
+           statusViewController.scheduleMessage("FIND A SURFACE TO PLACE AN OBJECT", inSeconds: 7.5, messageType: .planeEstimation)
+       }
+
+       // MARK: - Focus Square
+
+       func updateFocusSquare(isObjectVisible: Bool) {
+           if isObjectVisible || coachingOverlay.isActive {
+               focusSquare.hide()
+           } else {
+               focusSquare.unhide()
+               statusViewController.scheduleMessage("TRY MOVING LEFT OR RIGHT", inSeconds: 5.0, messageType: .focusSquare)
+           }
+           
+           // Perform ray casting only when ARKit tracking is in a good state.
+           if let camera = session.currentFrame?.camera, case .normal = camera.trackingState,
+               let query = sceneView.getRaycastQuery(),
+               let result = sceneView.castRay(for: query).first {
+               
+               updateQueue.async {
+                   self.sceneView.scene.rootNode.addChildNode(self.focusSquare)
+                   self.focusSquare.state = .detecting(raycastResult: result, camera: camera)
+               }
+               if !coachingOverlay.isActive {
+                   addObjectButton.isHidden = false
+               }
+               statusViewController.cancelScheduledMessage(for: .focusSquare)
+           } else {
+               updateQueue.async {
+                   self.focusSquare.state = .initializing
+                   self.sceneView.pointOfView?.addChildNode(self.focusSquare)
+               }
+               addObjectButton.isHidden = true
+               objectsViewController?.dismiss(animated: false, completion: nil)
+           }
+    }
+    
+    //       // MARK: - Error handling
+    //
+    //       func displayErrorMessage(title: String, message: String) {
+    //           // Blur the background.
+    //           blurView.isHidden = false
+    //
+    //           // Present an alert informing about the error that has occurred.
+    //           let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    //           let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+    //               alertController.dismiss(animated: true, completion: nil)
+    //               self.blurView.isHidden = true
+    //               self.resetTracking()
+    //           }
+    //           alertController.addAction(restartAction)
+    //           present(alertController, animated: true, completion: nil)
+    //       }
 }
 
 
