ca1132fbb5562416ff112eebab8f1b4ef0e8fea9
Martin Maly <martinmaly66@hotmail.com>
2020-07-08 23:41:21 -0400

    Added file to store units

--- Meine Klimaanlage AR/Model /ARModel/VirtualObject.swift
+++ Meine Klimaanlage AR/Model /ARModel/VirtualObject.swift
@@ -62,7 +62,7 @@ extension VirtualObject {
     // MARK: Static Properties and Methods
     /// Loads all the model objects within `Models.scnassets`.
     static let availableObjects: [VirtualObject] = {
-        let modelsURL = Bundle.main.url(forResource: "Models.scnassets", withExtension: nil)!
+        let modelsURL = Bundle.main.url(forResource: "ACUnits.scnassets", withExtension: nil)!
 
         let fileEnumerator = FileManager().enumerator(at: modelsURL, includingPropertiesForKeys: [])!
 
