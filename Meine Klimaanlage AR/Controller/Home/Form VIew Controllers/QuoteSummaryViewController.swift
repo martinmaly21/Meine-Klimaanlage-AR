660141abb2f67c447d3b099967aef827c3daa711
Martin Maly <martinmaly66@hotmail.com>
2020-07-12 14:28:26 -0400

    Added finish view controller

---
+++ Meine Klimaanlage AR/Controller/Home/Form VIew Controllers/QuoteSummaryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuoteSummaryViewController.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2020-07-12.
+//  Copyright © 2020 Tim Kohmann. All rights reserved.
+//
+
+import UIKit
+
+class QuoteSummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Meine Klimaanlage AR/View/Table View Cells/PreviewQuoteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PreviewQuoteTableViewCell.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2020-07-12.
+//  Copyright © 2020 Tim Kohmann. All rights reserved.
+//
+
+import UIKit
+
+class PreviewQuoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
