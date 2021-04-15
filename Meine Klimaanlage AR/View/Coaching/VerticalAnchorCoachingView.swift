328809b7e7ce23b534961986e8dba0c88b6546b9
Martin Maly <martinmaly66@hotmail.com>
2021-04-14 22:17:44 -0400

    Fixed scroll view for VerticalAnchorCoachingView.swift

--- Meine Klimaanlage AR/View/Coaching/VerticalAnchorCoachingView.swift
+++ Meine Klimaanlage AR/View/Coaching/VerticalAnchorCoachingView.swift
@@ -15,6 +15,8 @@ class VerticalAnchorCoachingView: UIView {
         "Tap the 'Place Unit' button below"
     ]
     
+    let videoTutorialScrollView = UIScrollView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUpUI()
@@ -25,6 +27,8 @@ class VerticalAnchorCoachingView: UIView {
     }
     
     private func setUpUI() {
+        isUserInteractionEnabled = true
+        
         backgroundColor = .white
         layer.cornerCurve = .continuous
         layer.cornerRadius = 15
@@ -33,32 +37,37 @@ class VerticalAnchorCoachingView: UIView {
         stackView.axis = .vertical
         stackView.distribution = .fill
         stackView.alignment = .center
+        stackView.clipsToBounds = true
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
         addSubview(stackView)
         
-        stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: trailingAnchor, constant: -30).isActive = true
-        stackView.topAnchor.constraint(equalTo: topAnchor, constant: 30).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
 //        stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -30).isActive = true
         
-        #warning("add video here? ")
         
-        
-        let videoTutorialScrollView = UIScrollView()
+        videoTutorialScrollView.clipsToBounds = true
+        videoTutorialScrollView.isScrollEnabled = true
+        videoTutorialScrollView.isPagingEnabled = true
+        videoTutorialScrollView.showsHorizontalScrollIndicator = false
         
         videoTutorialScrollView.translatesAutoresizingMaskIntoConstraints = false
         
         stackView.addArrangedSubview(videoTutorialScrollView)
+        videoTutorialScrollView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        videoTutorialScrollView.heightAnchor.constraint(equalTo: videoTutorialScrollView.widthAnchor).isActive = true
         
-        videoTutorialScrollView.heightAnchor.constraint(equalTo: widthAnchor).isActive = true
-        
+        //add content into scroll view
+        addViewsToScrollView()
         
         let showNextStepButton = UIButton()
         showNextStepButton.translatesAutoresizingMaskIntoConstraints = false
         showNextStepButton.setTitle("Show next step", for: .normal)
         showNextStepButton.setTitleColor(Constants.Color.primaryTextDark, for: .normal)
+        showNextStepButton.addTarget(self, action: #selector(userPressedShowNextStep), for: .touchUpInside)
         
         stackView.addArrangedSubview(showNextStepButton)
         
@@ -66,8 +75,43 @@ class VerticalAnchorCoachingView: UIView {
         skipTutorialButton.translatesAutoresizingMaskIntoConstraints = false
         skipTutorialButton.setTitle("Skip tutorial", for: .normal)
         skipTutorialButton.setTitleColor(Constants.Color.primaryTextDark, for: .normal)
+        skipTutorialButton.addTarget(self, action: #selector(userPressedSkipTutorial), for: .touchUpInside)
         
         stackView.addArrangedSubview(skipTutorialButton)
+    }
+    
+    private func addViewsToScrollView() {
+        var previousView: UIView?
         
+        for instruction in instructions {
+            let image = UIImage(named: "AppIcon")
+            let imageView = UIImageView(image: image)
+            imageView.clipsToBounds = true
+            imageView.isUserInteractionEnabled = true
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            
+            videoTutorialScrollView.addSubview(imageView)
+            imageView.heightAnchor.constraint(equalTo: videoTutorialScrollView.heightAnchor).isActive = true
+            imageView.widthAnchor.constraint(equalTo: videoTutorialScrollView.widthAnchor).isActive = true
+            imageView.centerYAnchor.constraint(equalTo: videoTutorialScrollView.centerYAnchor).isActive = true
+            
+            if let previousView = previousView {
+                imageView.leadingAnchor.constraint(equalTo: previousView.trailingAnchor).isActive = true
+                
+                if instruction == instructions.last {
+                    imageView.trailingAnchor.constraint(equalTo: videoTutorialScrollView.trailingAnchor).isActive = true
+                }
+            } else {
+                imageView.leadingAnchor.constraint(equalTo: videoTutorialScrollView.leadingAnchor).isActive = true
+            }
+            
+            previousView = imageView
+        }
+    }
+    
+    @objc func userPressedShowNextStep() {
+    }
+    
+    @objc func userPressedSkipTutorial() {
     }
 }
