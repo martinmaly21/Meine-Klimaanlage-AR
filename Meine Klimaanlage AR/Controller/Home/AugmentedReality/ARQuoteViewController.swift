55948290a9eba98600c46947390f7c24bb0c0b04
Martin Maly <martinmaly66@hotmail.com>
2021-04-16 22:31:43 -0400

    User can now pan AC unit!

--- Meine Klimaanlage AR/Controller/Home/AugmentedReality/ARQuoteViewController.swift
+++ Meine Klimaanlage AR/Controller/Home/AugmentedReality/ARQuoteViewController.swift
@@ -29,6 +29,12 @@ class ARQuoteViewController: UIViewController {
         return currentACUnit
     }
     
+    private var currentACUnitNode: SCNNode?
+    
+    //store previous coordinates from hittest to compare with current ones
+    private var previousPanCoordinate: CGPoint?
+    private var trackedObject: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpUI()
@@ -50,6 +56,74 @@ class ARQuoteViewController: UIViewController {
         
         // Prevent the screen from being dimmed to avoid interuppting the AR experience.
         UIApplication.shared.isIdleTimerDisabled = true
+        
+        //add gesture recognizers
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(userPannedScreen(_:)))
+        sceneView.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    @objc func userPannedScreen(_ panGesture: UIPanGestureRecognizer){
+        let location = panGesture.location(in: sceneView)
+        let acUnitBitMask = HitTestType.acUnit.rawValue
+        
+        switch panGesture.state {
+        case .began:
+            if let hitTestResult = sceneView.hitTest(
+                location,
+                options: [SCNHitTestOption.categoryBitMask : acUnitBitMask]
+            ).first {
+                //user is panning AC unit
+                trackedObject = currentACUnitNode
+                
+                previousPanCoordinate = CGPoint(
+                    x: Double(hitTestResult.worldCoordinates.x),
+                    y: Double(hitTestResult.worldCoordinates.y)
+                )
+            }
+        case .changed:
+            guard let trackedObject = trackedObject,
+                  let previousPanCoordinate = previousPanCoordinate else { return }
+            // when you start to pan in screen with your finger
+            // hittest gives new coordinates of touched location in sceneView
+            // coord-pcoord gives distance to move or distance paned in sceneview
+            if let hitTestResult = sceneView.hitTest(
+                location,
+                options: [SCNHitTestOption.categoryBitMask : acUnitBitMask]
+            ).first {
+                let coordx = hitTestResult.worldCoordinates.x
+                let coordy = hitTestResult.worldCoordinates.y
+                
+                let action = SCNAction
+                    .moveBy(
+                        x: CGFloat(coordx - Float(previousPanCoordinate.x)),
+                        y: CGFloat(coordy - Float(previousPanCoordinate.y)),
+                        z: 0,
+                        duration: 0.1
+                    )
+                
+                trackedObject.runAction(action)
+                
+                self.previousPanCoordinate = CGPoint(
+                    x: Double(coordx),
+                    y: Double(coordy)
+                )
+            }
+            
+            panGesture.setTranslation(CGPoint.zero, in: sceneView)
+        case .ended:
+            trackedObject = nil
+            previousPanCoordinate = nil
+        default:
+            break
+        }
+        
+    }
+    
+    private func removeGestureRecognizersFromView() {
+        //call when user has finished placing AC unit
+        for gestureRecognizer in sceneView.gestureRecognizers ?? [] {
+            sceneView.removeGestureRecognizer(gestureRecognizer)
+        }
     }
     
     private func setUpCoachingOverlay() {
@@ -128,9 +202,16 @@ extension ARQuoteViewController: ARCoachingOverlayViewDelegate {
                   let pointOfView = sceneView.pointOfView else {
                 fatalError("Could not get currentFrame or pointOfView")
             }
+            
+            
+            //set bit mask so it can be located in hit test
+            acUnit.categoryBitMask = HitTestType.acUnit.rawValue
+            
             let pointOfViewEulerAngle = pointOfView.eulerAngles
             
             acUnit.load()
+            
+            currentACUnitNode = acUnit
             
             let dimension: CGFloat = 2
             let plane = SCNPlane(width: dimension, height: dimension)
@@ -142,6 +223,7 @@ extension ARQuoteViewController: ARCoachingOverlayViewDelegate {
             let planeNode = SCNNode(geometry: plane)
             planeNode.transform = pointOfView.transform
             planeNode.eulerAngles = SCNVector3(0, pointOfViewEulerAngle.y, 0)
+            planeNode.categoryBitMask = HitTestType.plane.rawValue
             
             
             planeNode.addChildNode(acUnit)
---
+++ Meine Klimaanlage AR/Model /HitTestType.swift
@@ -0,0 +1,14 @@
+//
+//  HitTestType.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2021-04-16.
+//  Copyright © 2021 Tim Kohmann. All rights reserved.
+//
+
+import Foundation
+
+enum HitTestType: Int {
+    case acUnit = 0b0001
+    case plane = 0b0010
+}
