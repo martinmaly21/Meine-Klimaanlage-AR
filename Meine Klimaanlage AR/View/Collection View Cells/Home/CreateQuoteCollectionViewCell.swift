d367790b6103cb2fe89176ed067a64c2636e97eb
Martin Maly <martinmaly66@hotmail.com>
2021-05-01 20:09:57 -0400

    Added QuoteCollectionViewCell and CreateQuoteCollectionViewCell

---
+++ Meine Klimaanlage AR/View/Collection View Cells/Home/CreateQuoteCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CreateQuoteCollectionViewCell.swift
+//  Meine Klimaanlage AR
+//
+//  Created by Martin Maly on 2021-05-01.
+//  Copyright © 2021 Tim Kohmann. All rights reserved.
+//
+
+import UIKit
+
+class CreateQuoteCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
