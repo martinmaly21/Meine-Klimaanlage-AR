621f681f91c0b55f63fbe59de76c84b17871b73f
Martin Maly <martinmaly66@hotmail.com>
2021-05-02 13:39:54 -0400

    UI finessing

--- Meine Klimaanlage AR/Controller/Home/QuoteViewController.swift
+++ Meine Klimaanlage AR/Controller/Home/QuoteViewController.swift
@@ -35,7 +35,7 @@ class QuoteViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        tableView.reloadData()
+        tableView.reloadSections(IndexSet(integer: 1), with: .automatic)
     }
     
     private func registerTableViewCells() {
