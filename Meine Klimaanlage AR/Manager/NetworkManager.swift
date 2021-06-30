b02c84462a3c68174c26390b41937f81b7d2091c
Martin Maly <martinmaly66@hotmail.com>
2021-06-30 12:54:08 -0400

    Renamne Panasonic.scn to Panasonic_Inside_Unit.scn

--- Meine Klimaanlage AR/Manager/NetworkManager.swift
+++ Meine Klimaanlage AR/Manager/NetworkManager.swift
@@ -21,12 +21,12 @@ class NetworkManager {
                 //exterior
                 let wandgeratLowEnergy = ACUnit(
                     displayName: "Wandgerät Baureihe TZ 2.5kW",
-                    fileName: "Panasonic",
+                    fileName: "Panasonic_Inside_Unit",
                     environmentType: .interior
                 )
                 let wandgeratHighEnergy = ACUnit(
                     displayName: "Wandgerät Baureihe TZ 3.5kW",
-                    fileName: "Panasonic",
+                    fileName: "Panasonic_Inside_Unit",
                     environmentType: .interior
                 )
                 
